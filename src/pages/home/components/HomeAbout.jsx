13f427197092b3414c9fd68debafbbc3ac94e25f
rigottijp <rigottijp@gmail.com>
2023-11-23 18:03:36 -0300

    about

--- src/pages/home/components/HomeAbout.jsx
+++ src/pages/home/components/HomeAbout.jsx
@@ -1,29 +1,66 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import "./HomeAbout.css"
 import "../../../../src/styles.css"
 import { Link } from 'react-router-dom';
+import aboutImage from '../../../assets/Levin_frente_1024.jpg';
+
+import { homeAboutText } from "./homeAboutData";
+
 
 const HomeAbout = () => {
+
+  const [trunkLimit, setTrunkLimit] = useState(150);
+
+  const truncate = (description) => {
+    const words = description.split(' ');
+
+    if (words.length <= trunkLimit) {
+      return description;
+    } else {
+      const truncatedWords = words.slice(0, trunkLimit);
+      return truncatedWords.join(' ') + '...';
+    }
+  };
+
+  useEffect(() => {
+    const handleResize = () => {
+      if (window.innerWidth < 767) {
+        setTrunkLimit(150)
+      }if (window.innerWidth > 768 && window.innerWidth < 1200) {
+        setTrunkLimit(25)
+      } else {
+        setTrunkLimit(150)
+      }
+    };
+
+    window.addEventListener('resize', handleResize);
+    handleResize();
+
+    return () => {
+      window.removeEventListener('resize', handleResize);
+    };
+  }, []);
+
   return (
+
+
     <div className="main-container">
-      <div className='about-container mt-5 mb-5'>
-        <div className="about-text">
-          <p className='title'>Dra. María Elena Levín</p>
-          <p className='body mb-2'>Soy médica especializada en diagnóstico por imágenes.
-            Mi trayectoria incluye formación en Medicina Nuclear,
-            estudios internacionales en Tomografía Computada y Resonancia Magnética,
-            así como una dedicación continua a la práctica médica y la docencia.
-            Mi compromiso con la formación de futuros médicos y
-            la mejora de la atención médica ha sido una constante en mi carrera.
-            ¡Aprende más sobre mi historia y contribuciones en el campo de la medicina!
+      <div className="homeAbout mt-5 mb-5">
+        <div className="homeAbout--text">
+          <p className='homeAbout--text-title titlePri2'>Dra. María Elena Levín</p>
+          <p className='homeAbout--text-body text2 mb-2'>{truncate(homeAboutText.body)}
           </p>
 
-          < Link className="btn-1" to="/about" >Ver más</Link>
+          < Link className="btn1" to="/about" >Ver más</Link>
 
         </div>
-        <div className="about-image"></div>
+        <div className="homeAbout--image">
+          <img src={aboutImage} alt="Dra. Levin" />
+        </div>
       </div>
+
     </div>
+
   )
 }
 
--- src/pages/home/components/HomeServices.jsx
+++ src/pages/home/components/HomeServices.jsx
@@ -1,11 +1,12 @@
 import React from 'react'
+import "./HomeServices.css"
 
 const HomeServices = () => {
   return (
     <div className='main-container'>
-      <div className="services-container">
-          <p className='title'>Informes de Estudios a Distancia</p>
-          <p className='body'>Dirigido a: Instituciones médicas, médicos y pacientes para primera o segunda opinión. Estudios realizados en otros centros de diagnóstico.</p>
+      <div className="homeServices">
+          <p className='titlePri3'>Informes de Estudios a Distancia</p>
+          <p className='text1'>Dirigido a: Instituciones médicas, médicos y pacientes para primera o segunda opinión. Estudios realizados en otros centros de diagnóstico.</p>
       </div>
     </div>
   )
