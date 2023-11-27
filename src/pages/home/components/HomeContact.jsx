a581c27bf6e29d7a1ecde0c805cfa5c21ebcbd61
rigottijp <rigottijp@gmail.com>
2023-11-27 15:37:03 -0300

    agregamos nueva estructura de clases de CSS

--- src/pages/home/components/HomeContact.jsx
+++ src/pages/home/components/HomeContact.jsx
@@ -4,28 +4,36 @@ import { Link } from 'react-router-dom';
 
 const HomeContact = () => {
   return (
-    <div className='main-container mt-5 mb-5'>
-      <div className="homeContact-container">
-        <div className="cell-container container">
-        <div className="icon-container">
-            <span class="material-symbols-outlined google-icon">call</span>
-          </div>
-          <div className="text-container">
-            <p>11-1234-5678</p>
-          </div>
-        </div>
 
-        <div className="email-container container">
-          <div className="icon-container">
-            <span class="material-symbols-outlined google-icon">mail</span>
-          </div>
-          <div className="text-container">
-            < Link className="link-to" to="/contact" >Envie su consulta</Link>
-          </div>
-        </div>
 
+
+    <div className="homeContact  mt-5 mb-5">
+      <div className="main-container">
+        <div className="homeContact--container">
+
+          <div className="homeContact__cell">
+            <div className="homeContact__cell__icon">
+              <span class="material-symbols-outlined google-icon">call</span>
+            </div>
+            <div className="homeContact__cell__text">
+              <p>11-1234-5678</p>
+            </div>
+          </div>
+
+          <div className="homeContact__email">
+            <div className="homeContact__email__icon">
+              <span class="material-symbols-outlined google-icon">mail</span>
+            </div>
+            <div className="homeContact__email__text">
+              < Link className="link-to" to="/contact" >Envie su consulta</Link>
+            </div>
+          </div>
+
+        </div>
       </div>
+
     </div>
+
   )
 }
 
