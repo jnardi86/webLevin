94d03b9bc1db4995aefc7da2d0023d301a9ebd65
informaticasantojanni <informatica.santojanni@gmail.com>
2023-11-27 18:26:22 -0300

    Add new Styles NavBar

--- src/components/navbar/components/NavDesktop.jsx
+++ src/components/navbar/components/NavDesktop.jsx
@@ -9,15 +9,20 @@ const NavDesktop = () => {
 
   return (
     <nav>
-      <div className="nav-desktop-body">
-        <ul>
-          {navItems.map((item, index) => {
-            return (
-              <li key={index}>
-                <Link className="linkTo" to={item.link}>{item.label}</Link></li>
-            )
-          })}
-        </ul>
+      <div className="navDesktop--container">
+        <div className="navDesktop__logo">
+          <p className='titlePri3'><strong>Dra.</strong> María Elena<strong> Levín</strong></p>
+        </div>
+        <div className="navDesktop__links">
+          <ul>
+            {navItems.map((item, index) => {
+              return (
+                <li key={index}>
+                  <Link className="linkTo textLinks" to={item.link}>{item.label}</Link></li>
+              )
+            })}
+          </ul>
+        </div>
       </div>
     </nav>
   )
