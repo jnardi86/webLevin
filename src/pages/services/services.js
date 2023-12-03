50f3cfe3ac674f0e7be82cdc8a9729d52bb2831f
jnardi86 <julian.nardi@gmail.com>
2023-12-03 20:12:54 -0300

    Add page Services

---
+++ src/pages/services/services.js
@@ -0,0 +1,18 @@
+export const services = [
+    {  
+      title: "Service1",
+      description: "Descripcion servicio1",
+    },
+    {
+      label: "Service2",
+      description: "Descripcion servicio2",
+    },
+    {
+      label: "Service3",
+      description: "Descripcion servicio3",
+    },
+    {
+      label: "Service4",
+      description: "Descripcion servicio4",
+    },
+  ]
