cff873b98b4994345768a6b7d2de25aede9860e3
informaticasantojanni <informatica.santojanni@gmail.com>
2023-12-04 15:19:21 -0300

    Add new component ServicesList

---
+++ src/pages/services/components/ServicesList.jsx
@@ -0,0 +1,44 @@
+import Service from '../components/Service'
+import ServicePhoto from '../components/ServicePhoto'
+import serviceImg1 from '../../../assets/service_1.png'
+import serviceImg2 from '../../../assets/service_2.png'
+import serviceImg3 from '../../../assets/service_3.png'
+import './servicesList.css'
+
+const ServicesList = () => {
+  return (
+    <div className='services'>
+    <div className="services__header">
+      <h2 className='titleSec2'>Servicios</h2>
+    </div>
+    <div className='main-container'>
+      <div className="services__body">
+        <Service
+          title='Servicio_1'
+          description='Esta es la descripción del servicio 1'
+        />
+        <ServicePhoto
+          img={serviceImg1}
+        />
+        <Service
+          title='Servicio_2'
+          description='Esta es la descripción del servicio 2'
+        />
+        <ServicePhoto
+          img={serviceImg2}
+        />
+        <Service
+          title='Servicio_3'
+          description='Esta es la descripción del servicio 3'
+        />
+        <ServicePhoto
+          img={serviceImg3}
+        />
+      </div>
+    </div>
+
+  </div>
+  )
+}
+
+export default ServicesList
