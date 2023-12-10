cee67a7470d428bb97d5ee6392ec44414d6481be
jnardi86 <julian.nardi@gmail.com>
2023-12-10 18:45:45 -0300

    New changes in Services Page

--- src/pages/services/components/ServicesList.jsx
+++ src/pages/services/components/ServicesList.jsx
@@ -1,6 +1,12 @@
 import Service from '../components/Service'
 import ServicePhoto from '../components/ServicePhoto'
 import serviceMobile1 from '../../../assets/services/service_1_mobile.png'
+import serviceDesktop1 from '../../../assets/services/service_1_desktop.png'
+import serviceMobile2 from '../../../assets/services/service_2_mobile.png'
+import serviceDesktop2 from '../../../assets/services/service_2_desktop.png'
+import serviceMobile3 from '../../../assets/services/service_3_mobile.png'
+import serviceDesktop3 from '../../../assets/services/service_3_desktop.png'
+
 import './servicesList.css'
 
 const ServicesList = () => {
@@ -8,13 +14,42 @@ const ServicesList = () => {
 
     <div className="main-container">
       <div className="servicesList mt-5 mb-5">
-        <Service
-        title='Informes a distancia'
-        description='Informes de imágenes vía email certificado por la dra. Levín'
-        />
-        <ServicePhoto
-        imgMobile={serviceMobile1}
-        />
+        <div className="serviceList--item1">
+          <Service
+            title='Telediagnóstico'
+            description='Con nuestro servicio de telediagnóstico, la Dra. María Levin proporciona informes y diagnósticos de estudios a distancia. La eficiencia y la precisión son fundamentales para ofrecer un soporte rápido y de calidad a los profesionales de la salud.'
+          />
+        </div>
+        <div className="serviceList--item2">
+          <ServicePhoto
+            imgMobile={serviceMobile1}
+            imgDesktop={serviceDesktop1}
+          />
+        </div>
+        <div className="serviceList--item3">
+          <Service
+          title='Resonancia Magnética'
+          description='Ofrecemos servicios avanzados de resonancia magnética para obtener imágenes detalladas de estructuras internas. Nuestro equipo altamente capacitado garantiza estudios precisos y análisis detallados para respaldar el diagnóstico médico.'
+          />
+        </div>
+        <div className="serviceList--item4">
+          <ServicePhoto
+          imgMobile={serviceMobile2}
+          imgDesktop={serviceDesktop2}
+          />
+        </div>
+        <div className="serviceList--item5">
+          <Service
+          title='Tomografía Computada'
+          description='Realizamos estudios de tomografía computada de alta calidad para obtener imágenes detalladas de órganos y tejidos. Nuestros informes precisos y confiables contribuyen al diagnóstico preciso y al plan de tratamiento adecuado.'
+          />
+        </div>
+        <div className="serviceList--item6">
+          <ServicePhoto
+          imgMobile={serviceMobile3}
+          imgDesktop={serviceDesktop3}
+          />
+        </div>
       </div>
     </div>
 
